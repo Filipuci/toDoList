6d80def54da133ec0a3e562b932d98303eb3c539
Filipuci <rafaellifipuci@outlook.com>
2025-05-20 02:24:42 -0300

    adding logic of input.value and verification

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,9 @@ const input = document.querySelector('.taskText')
 
 input.addEventListener('keydown', e => {
   if(e.key === 'Enter' && input.value !== '') {
-    document.querySelector('#tasks').innerHTML = `<label class="tasks"> <input type="checkbox"> ${input.value} </label>`
-  }
+    document.querySelector('#tasks').innerHTML += `<label class="tasks"> <input type="checkbox"> ${input.value} </label>`
+    input.value = ''
+  } 
 })
 
 document.querySelectorAll('.task').forEach(task => task.addEventListener('click', e => {
