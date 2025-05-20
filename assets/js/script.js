71aa6e098ded3e6d4bc01861666730d441fe4d3c
Filipuci <rafaellifipuci@outlook.com>
2025-05-20 02:15:48 -0300

    adding events with js and more styles

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('.taskText')
+
+input.addEventListener('keydown', e => {
+  if(e.key === 'Enter' && input.value !== '') {
+    document.querySelector('#tasks').innerHTML = `<label class="tasks"> <input type="checkbox"> ${input.value} </label>`
+  }
+})
+
+document.querySelectorAll('.task').forEach(task => task.addEventListener('click', e => {
+  e.target.classList.toggle('completed-task')
+}))
