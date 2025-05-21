81ad67399d1c187585f0eda1009bf1025b028abe
Filipuci <rafaellifipuci@outlook.com>
2025-05-20 22:58:33 -0300

    fixing completed-task event click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
 const input = document.querySelector('.taskText')
 const tasks = document.querySelector('#tasks')
 
+
+// Adiciona as tarefas com o input
 input.addEventListener('keydown', e => {
-  if(e.key === 'Enter' && input.value !== '') {
+  if (e.key === 'Enter' && input.value !== '') {
     tasks.innerHTML += `<label class="task"> <input type="checkbox"> <span class="inputValue">${input.value}</span> </label>`
     input.value = ''
-  } 
+  }
 })
 
+// Risca a tarefa concluída
 tasks.addEventListener('click', e => {
-  if(e.target.classList.contains('task')) {
-    e.target.classList.toggle('completed-task')
+  if (e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
+    e.target.closest('.task').classList.toggle('completed-task')
   }
+})
+
+document.querySelector('#completed-btn').addEventListener('click', () => {
+  let completedTasks = 
+  tasks.innerHTML = ''
+  console.log(tasks)
 })
