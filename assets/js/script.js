1f6d3e0906425842679344509e02d1b08a2631c0
Filipuci <rafaellifipuci@outlook.com>
2025-06-11 22:14:52 -0300

    adding delete button, improving js with create element and dataset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
 const input = document.querySelector('.taskText')
 const tasks = document.querySelector('#tasks')
-
+let countTasks = 0
 
 // Adiciona as tarefas com o input
 input.addEventListener('keydown', e => {
-  if (e.key === 'Enter' && input.value !== '') {
-    tasks.innerHTML += `<label class="task"> <input type="checkbox"> <span class="inputValue">${input.value}</span> </label>`
+  if (e.key === 'Enter' && input.value.trim() !== '') {
+
+    const li = document.createElement('li')
+    li.dataset.status = 'pending'
+
+    const label = document.createElement('label')
+    label.setAttribute('for', `task-${countTasks}`)
+    label.classList.add('inputValue')
+    label.textContent = input.value
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.setAttribute('id', `task-${countTasks}`)
+
+    const deleteTask = document.createElement('span') 
+    deleteTask.classList.add('delete-btn')
+    deleteTask.innerHTML = '<i class="fa-solid fa-trash"></i>'
+
+    li.appendChild(checkbox)
+    li.appendChild(label)
+    li.appendChild(deleteTask)
+    tasks.appendChild(li)
+
+    countTasks++
     input.value = ''
   }
 })
@@ -13,35 +35,49 @@ input.addEventListener('keydown', e => {
 // Risca a tarefa concluída
 tasks.addEventListener('click', e => {
   if (e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
-    e.target.closest('.task').classList.toggle('completed-task')
+    console.log('cliquei no input')
+    const task = e.target.closest('[data-status]')
+    task.classList.toggle('completed-task')
+
+    task.dataset.status = task.classList.contains('completed-task') ? 'completed' : 'pending'
   }
 })
 
+// Deleta a tarefa
+tasks.addEventListener('click', e => {
+  if (e.target.closest('.delete-btn')) {
+    e.target.closest('[data-status]').remove()
+  }})
+
+function getAllTasks() {
+  return Array.from(document.querySelectorAll('[data-status]'))
+}
+
+// Mostra pro usuário as tarefas completas
 document.querySelector('#completed-btn').addEventListener('click', () => {
-  const task = document.querySelectorAll('.task')
-  Array.from(task).forEach(tarefa => {
-    if (tarefa.classList.contains('completed-task')) {
-      tarefa.style.display = 'flex'
+  getAllTasks().forEach(item => {
+    if (item.dataset.status === 'completed') {
+      item.style.display = 'flex'
     } else {
-      tarefa.style.display = 'none'
+      item.style.display = 'none'
     }
   })
 })
 
+// Mostra pro usuário as tarefas pendentes
 document.querySelector('#pending-btn').addEventListener('click', () => {
-  const task = document.querySelectorAll('.task')
-  Array.from(task).forEach(tarefa => {
-    if (tarefa.classList.contains('completed-task')) {
-      tarefa.style.display = 'none'
+  getAllTasks().forEach(item => {
+    if (item.dataset.status === 'pending') {
+      item.style.display = 'flex'
     } else {
-      tarefa.style.display = 'flex'
+      item.style.display = 'none'
     }
   })
 })
 
+// Mostra pro usuário todas as tarefas
 document.querySelector('#allTasks-btn').addEventListener('click', () => {
-  const task = document.querySelectorAll('.task')
-  Array.from(task).forEach(tarefa => {
-    tarefa.style.display = 'flex'
+  getAllTasks().forEach(item => {
+    item.style.display = 'flex'
   })
 })
